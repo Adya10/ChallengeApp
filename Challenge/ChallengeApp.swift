bea9f29c1bcbe178fb1f1ebbd352f130b0b05892
Advait Lanjekar <adya.lanjekar@gmail.com>
2023-10-17 14:30:41 +0200

    Initial Commit

---
+++ Challenge/ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChallengeApp.swift
+//  Challenge
+//
+//  Created by Advait Lanjekar on 17/10/23.
+//
+
+import SwiftUI
+
+@main
+struct ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
