1e6e5f94190537a6cfeb950c6f4c3b217ea9c3d2
Advait Lanjekar <adya.lanjekar@gmail.com>
2023-10-17 14:59:11 +0200

    Completed challenge

--- Challenge/ContentView.swift
+++ Challenge/ContentView.swift
@@ -8,14 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    //5 random words
+    var words = ["Emu","Adi","Megamind","Apple","Banana"]
+    
+    //will store the randomly generated item
+    @State var random = [String]()
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            List(random, id: \.self){ random in
+                Text(random)
+            
+            }
+            Button("Button") {
+                var number = Int.random(in: 0...4)
+                random.append(words[number])
+            }
         }
-        .padding()
     }
 }
 
